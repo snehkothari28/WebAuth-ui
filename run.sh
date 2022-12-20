e79e53afcba21e449ea3dd6042033cb9745c4972
snehkothari28 <sneh.kothari28@gmail.com>
2022-12-21 03:54:13 +0530

    Adding shell scripts for automation

--- run.sh
+++ run.sh
@@ -4,11 +4,13 @@ if test -f nohup.out; then
   rm nohup.out
 fi
 
+npm install 
+
 timestamp=$(date "+%Y%m%d%H%M%S")
 [ ! -d server-logs ] && mkdir server-logs
 [ ! -d server-logs/"$timestamp" ] && mkdir server-logs/"$timestamp"
 
-nohup ng serve --configuration=production --port=4401 1>./server-logs/"$timestamp"/stdout.txt 2>./server-logs/"$timestamp"/stderr.txt &
+nohup ng serve --configuration=production 1>./server-logs/"$timestamp"/stdout.txt 2>./server-logs/"$timestamp"/stderr.txt &
 echo $! > save_pid.txt
 
 echo "Running WebAuth-ui server and logging at server-logs/${timestamp}"
