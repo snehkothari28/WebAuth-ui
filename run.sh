2e5b6f6cf5a290fc1e236939d20d34085dd6ab18
snehkothari28 <sneh.kothari28@gmail.com>
2022-12-21 03:20:13 +0530

    Adding shell scripts for automation

--- run.sh
+++ run.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-rm nohup.out
-nohup ng serve --configuration=production --port=4401 1>./server-logs/stdout.txt 2>./server-logs/stderr.txt &
+if test -f nohup.out; then
+  rm nohup.out
+fi
+
+timestamp=$(date "+%Y%m%d%H%M%S")
+mkdir server-logs/timestamp
+
+nohup ng serve --configuration=production --port=4401 1>./server-logs/timestamp/stdout.txt 2>./server-logs/timestamp/stderr.txt &
 echo $! > save_pid.txt
