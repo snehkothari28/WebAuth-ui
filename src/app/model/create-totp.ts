8414fbe6dd6ecd73103909fc8a42e758138cd37b
vidhi202 <111477063+vidhi202@users.noreply.github.com>
2023-03-17 15:50:54 +0530

    Update create-totp.ts

--- src/app/model/create-totp.ts
+++ src/app/model/create-totp.ts
@@ -4,7 +4,6 @@ import { DelegationModel } from './delegation-model';
 export interface CreateTOTP {
   id: Number | undefined;
   name: string;
-  // type:typeList;
   type:string;
   secretKey: string | undefined;
   url: string;
