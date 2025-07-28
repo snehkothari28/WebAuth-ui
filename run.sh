31d6a399ab981bcf55140f60eba6822ae4f97d3d
Sneh Kothari <sneh.kothari28@gmail.com>
2025-07-29 03:13:37 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -10,7 +10,12 @@ timestamp=$(date "+%Y%m%d%H%M%S")
 [ ! -d server-logs ] && mkdir server-logs
 [ ! -d server-logs/"$timestamp" ] && mkdir server-logs/"$timestamp"
 
-nohup ng serve --configuration=production --host=0.0.0.0 --disable-host-check> ./server-logs/"$timestamp"/log.txt 2>&1 &
+# nohup ng serve --configuration=production --host=0.0.0.0 --disable-host-check> ./server-logs/"$timestamp"/log.txt 2>&1 &
+nohup node --max-old-space-size=1024 ./node_modules/@angular/cli/bin/ng serve \
+  --configuration=production \
+  --host=0.0.0.0 \
+  --disable-host-check \
+  > ./server-logs/"$timestamp"/log.txt 2>&1 &
 echo $! > save_pid.txt
 
 echo "Running WebAuth-ui server and logging at server-logs/${timestamp}"
