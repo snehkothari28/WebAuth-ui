44a270b2ee82f7b4535191e4cef80cc9499929b7
Vidhi Shah <vidhishah221@gmail.com>
2025-10-27 22:35:46 +0530

    refactor: streamline authentication method and improve HTTP request handling

--- src/app/authentication.service.ts
+++ src/app/authentication.service.ts
@@ -1,16 +1,24 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
+import { environment } from '../environments/environment'; 
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthenticationService {
-  
-  constructor() {} 
+  private backendUrl = environment.backEndUrl;
+
+  constructor(private http: HttpClient) {}
 
   authenticate(jwt: string): Observable<any> {
     sessionStorage.setItem('token', jwt);
-    return of({ success: true });
+
+    const headers = new HttpHeaders({
+      Authorization: `Bearer ${jwt}`,
+    });
+
+    return this.http.get(`${this.backendUrl}authenticate`, { headers });
   }
 
   isLoggedIn(): Observable<boolean> {
